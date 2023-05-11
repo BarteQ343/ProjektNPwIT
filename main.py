9e5944abdf938ed504c9e8521c026f4fd283fab2
BarteQ343 <bartoszchmiel72@gmail.com>
2023-05-11 12:01:53 +0200

    Przygodowanie do połączenia z kodem konwersji, poprzez dodanie logiki kontrolującej konwersję.

--- main.py
+++ main.py
@@ -47,7 +47,6 @@ class Window(QMainWindow):
             }
         """)
 
-
         max_width = int(self.width() * 0.95)
 
         big_label = QLabel("Konwerter plików XML, YML, JSON", widget)
@@ -63,9 +62,12 @@ class Window(QMainWindow):
         self.button.setMaximumWidth(max_width)
         self.button.clicked.connect(self.openFileDialog)
 
-        checkBoxXML = QCheckBox("Konwertuj na XML", widget)
-        checkBoxYML = QCheckBox("Konwertuj na YML", widget)
-        checkBoxJSON = QCheckBox("Konwertuj na JSON", widget)
+        self.checkBoxXML = QCheckBox("Konwertuj na XML", widget)
+        self.checkBoxYML = QCheckBox("Konwertuj na YML", widget)
+        self.checkBoxJSON = QCheckBox("Konwertuj na JSON", widget)
+        self.checkBoxXML.setEnabled(False)
+        self.checkBoxJSON.setEnabled(False)
+        self.checkBoxYML.setEnabled(False)
 
         self.small_label2 = QLabel("Obecnie wybrany katalog zapisu:\n"+ROOT_DIR, widget)
         self.small_label2.setAlignment(Qt.AlignCenter)
@@ -74,6 +76,11 @@ class Window(QMainWindow):
         self.button2 = QPushButton("Przeglądaj...", widget)
         self.button2.setMaximumWidth(max_width)
         self.button2.clicked.connect(self.openFolderDialog)
+
+        
+        self.button3 = QPushButton("Konwertuj", widget)
+        self.button3.setMaximumWidth(max_width)
+        self.button3.clicked.connect(self.Convert)
 
         self.small_label3 = QLabel("Placeholder", widget)
         self.small_label3.setAlignment(Qt.AlignCenter)
@@ -95,9 +102,9 @@ class Window(QMainWindow):
         sub_layout1.addWidget(self.small_label)
         sub_layout1.addWidget(self.button)
         checkBoxLayout = QVBoxLayout()
-        checkBoxLayout.addWidget(checkBoxXML)
-        checkBoxLayout.addWidget(checkBoxYML)
-        checkBoxLayout.addWidget(checkBoxJSON)
+        checkBoxLayout.addWidget(self.checkBoxXML)
+        checkBoxLayout.addWidget(self.checkBoxYML)
+        checkBoxLayout.addWidget(self.checkBoxJSON)
         sub_layout1.addLayout(checkBoxLayout)
         sub_layout1.setContentsMargins(10, 0, 10, 10)
         sub_layout1.setSpacing(10)
@@ -106,6 +113,7 @@ class Window(QMainWindow):
         sub_layout2 = QVBoxLayout()
         sub_layout2.addWidget(self.small_label2)
         sub_layout2.addWidget(self.button2)
+        sub_layout2.addWidget(self.button3)
         sub_layout2.setContentsMargins(10, 0, 10, 10)
         sub_layout2.setSpacing(10)
         v_layout.addLayout(sub_layout2)
@@ -127,7 +135,7 @@ class Window(QMainWindow):
         options |= QFileDialog.ShowDirsOnly
         global fileDir
         fileDir = ROOT_DIR
-        fileDirTmp = QFileDialog.getExistingDirectory(self,"Choose Save Directory", "", options=options)
+        fileDirTmp = QFileDialog.getExistingDirectory(self,"Wybierz miejsce zapisu", "", options=options)
         if fileDirTmp == "":
             fileDirLoc = "Obecnie wybrany katalog zapisu:\n"+ROOT_DIR
             self.small_label2.setText(fileDirLoc)
@@ -138,9 +146,59 @@ class Window(QMainWindow):
 
     def openFileDialog(self):
         options = QFileDialog.Options()
-        file = QFileDialog.getExistingDirectory(self,"Wybierz plik do konwersji", "", options=options)
-        fileLoc = "Obecnie wybrany plik:\n"+file
-        self.small_label.setText(fileLoc)
+        filter = "Config files (*.xml *.json *.yml *.yaml)"
+        fileTmp = QFileDialog.getOpenFileName(self,"Wybierz plik do konwersji", "", filter, options=options)
+        fileTmp = ','.join(map(str, (fileTmp))).split(',')[0]
+        if fileTmp == "":
+            fileLoc = "Nie wybrano żadnego pliku\n"
+            self.small_label.setText(fileLoc)
+        else:
+            self.file = fileTmp
+            fileLoc = "Obecnie wybrany plik:\n"+self.file
+            self.small_label.setText(fileLoc)
+            fileExtention = self.file.split('.')[-1]
+            if fileExtention == 'xml':
+                self.checkBoxXML.setEnabled(False)
+                self.checkBoxJSON.setEnabled(True)
+                self.checkBoxYML.setEnabled(True)
+            elif fileExtention == 'json':
+                self.checkBoxXML.setEnabled(True)
+                self.checkBoxJSON.setEnabled(False)
+                self.checkBoxYML.setEnabled(True)
+            else:
+                self.checkBoxXML.setEnabled(True)
+                self.checkBoxJSON.setEnabled(True)
+                self.checkBoxYML.setEnabled(False)
+
+    def Convert(self):
+        if hasattr(self, 'file'):
+            file = self.file
+            print(file)
+            self.small_label3.setText("")
+            self.small_label4.setText("")
+            if self.checkBoxJSON.isChecked() == False and self.checkBoxXML.isChecked() == False and self.checkBoxYML.isChecked() == False:
+                self.small_label3.setText("Wybierz na co przekonwertować plik!")
+            else:
+                if self.checkBoxXML.isChecked():
+                    self.small_label3.setText("XML_True")
+                else:
+                    print("XML_False")
+                if self.checkBoxYML.isChecked():
+                    if self.small_label3.text() == "XML_True":
+                        self.small_label4.setText("YML_True")
+                    else:
+                        self.small_label3.setText("YML_True")
+                else:
+                    print("YML_False")
+                if self.checkBoxJSON.isChecked():
+                    if self.small_label3.text() == "":
+                        self.small_label3.setText("JSON_True")
+                    else:
+                        self.small_label4.setText("JSON_True")
+                else:
+                    print("JSON_False")
+        else:
+            self.small_label.setText("Wybierz najpierw plik do konwersji")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
